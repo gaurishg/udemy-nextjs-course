aea44599b5963cc83ad291cd37270a4d41727b95
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-27 13:39:22 +0000

    Added event search filter component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,12 +3,22 @@ import classes from './button.module.scss';
 import Link from 'next/link';
 
 interface Props {
-    link: string;
+    link?: string;
+    onClick?: () => void;
     children: ReactNode
 }
 
-export default function Button({link, children}: Props) {
-    return <Link href={link} className={classes.btn}>
+export default function Button({link, children, onClick}: Props) {
+    if (!link && !onClick)
+        throw new Error('Set either link or onClick on the Button, both cant be left');
+    
+    if (link) {
+        return <Link href={link} className={classes.btn}>
+            {children}
+        </Link>;
+    }
+
+    return <button className={classes.btn} onClick={onClick}>
         {children}
-    </Link>;
+    </button>;
 }
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,10 +1,12 @@
 import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
 import { getAllEvents } from "@/dummy-data";
 
 export default function AllEventsPage() {
     const events = getAllEvents();
 
-    return <div>
+    return <>
+        <EventsSearch />
         <EventList items={events}/>
-    </div>;
+    </>;
 }
