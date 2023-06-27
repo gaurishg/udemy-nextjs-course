6ac08936200c4cbaad086c8dc46654f01855bb2c
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-27 14:36:25 +0000

    added onSearch to EventsSearch

--- components/events/events-search.tsx
+++ components/events/events-search.tsx
@@ -1,19 +1,35 @@
+import { FormEventHandler, useRef } from 'react';
 import Button from '../ui/button';
 import classes from './events-search.module.scss';
 
-export default function EventsSearch() {
-    return <form className={classes.form}>
+interface Props {
+    onSearch: (year: string, month: string) => void;
+}
+
+export default function EventsSearch(props: Props) {
+    const yearInputRef = useRef<HTMLSelectElement>(null);
+    const monthInputRef = useRef<HTMLSelectElement>(null);
+    
+    const submitHander: FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+
+        const selectedYear = yearInputRef.current!.value;
+        const selectedMonth = monthInputRef.current!.value;
+        props.onSearch(selectedYear, selectedMonth);
+    }
+
+    return <form className={classes.form} onSubmit={submitHander}>
         <div className={classes.controls}>
             <div className={classes.control}>
                 <label htmlFor="year">Year</label>
-                <select name="year" id="year">
+                <select name="year" id="year" ref={yearInputRef}>
                     <option value="2021">2021</option>
                     <option value="2022">2022</option>
                 </select>
             </div>
             <div className={classes.control}>
                 <label htmlFor="month">Month</label>
-                <select name="month" id="month">
+                <select name="month" id="month" ref={monthInputRef}>
                     <option value="1">January</option>
                     <option value="2">February</option>
                     <option value="3">March</option>
@@ -29,6 +45,6 @@ export default function EventsSearch() {
                 </select>
             </div>
         </div>
-        <Button onClick={() => {}}>Find Events</Button>
+        <Button type="submit">Find Events</Button>
     </form>;
 }
