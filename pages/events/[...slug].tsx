6b36e96bd5c1a8a3ed400b87fce70afd4f34da95
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-27 15:08:42 +0000

    filters and routes setup

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,5 +1,54 @@
+import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/events/results-title";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 export default function FilteredEventsPage() {
+    const router = useRouter();
+
+    const filterData = router.query.slug;
+    console.log(filterData);
+
+    if (!filterData) {
+        return <p className="center">Loading...</p>
+    }
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+        return <>
+            <ErrorAlert>
+                <p>Invalid filter values</p>
+            </ErrorAlert>
+            <div className="center">
+                <Button link="/events">Show All Events</Button>
+            </div>
+        </>;
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth
+    });
+
+    if (!filteredEvents.length) {
+        return <>
+            <ErrorAlert>
+                <p>No events found for the chosen filter</p>
+            </ErrorAlert>
+            <div className="center">
+                <Button link="/events">Show All Events</Button>
+            </div>
+        </>;
+    }
     return <>
-        <h1>Filtered Events</h1>
+        <ResultsTitle date={new Date(numYear, numMonth - 1)} />
+        <EventList items={filteredEvents} />
     </>;
 }
--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,6 +1,7 @@
 import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
+import ErrorAlert from "@/components/ui/error-alert";
 import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
 
@@ -11,12 +12,16 @@ export default function EventDetailPage() {
     const event = getEventById(eventId);
 
     if (!event) {
-        return <>No event found!</>;
+        return <>
+            <ErrorAlert>
+                No event found!
+            </ErrorAlert>
+        </>;
     }
 
     return <>
-        <EventSummary title={event.title}/>
-        <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+        <EventSummary title={event.title} />
+        <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
         <EventContent>
             <p>{event.description}</p>
         </EventContent>
