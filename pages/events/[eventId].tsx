3d644342a8bc39b8a39f18f3ec9b58c05be77b27
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-27 13:11:23 +0000

    individual event pages done

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,5 +1,24 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 export default function EventDetailPage() {
+    const router = useRouter();
+
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return <>No event found!</>;
+    }
+
     return <>
-        <h1>Event Detail</h1>
+        <EventSummary title={event.title}/>
+        <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+        <EventContent>
+            <p>{event.description}</p>
+        </EventContent>
     </>;
 }
